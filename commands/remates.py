a0316737f9a8eba747178f6dc9e0f89cb4ab423e
Bryan Oxley <bryanoxley1999@gmail.com>
2021-06-25 17:12:22 -0300

    Ajustada la fecha para que cambie a UTF-3 (quitandole 3 a la hora de heroku)

--- commands/remates.py
+++ commands/remates.py
@@ -39,7 +39,7 @@ def crear_remate(message):
             if len(datos[4]) > 6 and datos[4][8] == '/' and datos[4][11] == '/' and datos[4][14] == ' ' and datos[4][17] == ':':
                 ahora = datetime.now().strftime('%d/%m/%y %H:%M').split(' ')
                 final = str(datos[4][6:])
-                if int(ahora[0].split('/')[0]) >= int(final.split(' ')[0].split('/')[0]) and int(ahora[0].split('/')[1]) >= int(final.split(' ')[0].split('/')[1]) and int(ahora[0].split('/')[2]) >= int(final.split(' ')[0].split('/')[2]) and int(ahora[1].split(':')[0]) >= int(final.split(' ')[1].split(':')[0]) and int(ahora[1].split(':')[1]) + 10 >= int(final.split(' ')[1].split(':')[1]):
+                if int(ahora[0].split('/')[0]) >= int(final.split(' ')[0].split('/')[0]) and int(ahora[0].split('/')[1]) >= int(final.split(' ')[0].split('/')[1]) and int(ahora[0].split('/')[2]) >= int(final.split(' ')[0].split('/')[2]) and int(ahora[1].split(':')[0]) - 3 >= int(final.split(' ')[1].split(':')[0]) and int(ahora[1].split(':')[1]) + 10 >= int(final.split(' ')[1].split(':')[1]):
                     embed = discord.Embed(
                         title='ERROR EN FECHA',
                         description='Es obligatorio escribir una fecha y hora de finalización futura, no puede haber pasado ya',
