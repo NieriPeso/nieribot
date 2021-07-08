320324fb74470f8d3d4e1cc9b5632da3de56ce82
BryanOx <bryanoxley1999@gmail.com>
2021-07-07 23:44:20 -0300

    Import crash solved

--- commands/validation.py
+++ commands/validation.py
@@ -1,4 +1,9 @@
-from . import channels
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from utils.constants import channels
 
 def validate_permissions(ctx):
     if "admins" in [y.name.lower() for y in ctx.author.roles] or "ñod" in [y.name.lower() for y in ctx.author.roles]:
@@ -8,6 +13,6 @@ def validate_permissions(ctx):
 def validate_channel(channel, key):
     
     # VALIDAR CANAL SEGUN LA KEY PASADA
-    if channel == channels.channels[key]:
+    if channel == channels[key]:
         return True
     return False
